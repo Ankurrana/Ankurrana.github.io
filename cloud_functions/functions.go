a84e3b0a8a65dfa8f04e2f32e1b9e47024b905b1
Ankur Rana <ankurofficial@hotmail.com>
2024-08-29 11:50:38 +0530

    using a single cloud function to upload/download

--- cloud_functions/functions.go
+++ cloud_functions/functions.go
@@ -4,15 +4,38 @@ import (
 	"bytes"
 	"context"
 	"fmt"
+	"html/template"
 	"io"
 	"net/http"
 
 	"cloud.google.com/go/storage"
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
+	"google.golang.org/api/iterator"
 )
 
 func init() {
-	functions.HTTP("upload", uploadHandler)
+	functions.HTTP("dynamic", dynamicHandler)
+}
+
+func dynamicHandler(w http.ResponseWriter, r *http.Request) {
+
+	function := r.URL.Query().Get("func")
+
+	// Extract the filename from the path
+	switch function {
+	case "upload":
+		uploadHandler(w, r)
+		return
+	case "download":
+		downloadHandler(w, r)
+		return
+	default:
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "incorrect func, you use correct format for the url /dynamic?func=upload or /dynamic?func=download or /dynamic?func=download&filename=xyz")
+	}
+
+	// Use the filename variable as needed
+
 }
 
 // https://us-central1-ankur-personal.cloudfunctions.net/upload
@@ -84,4 +107,102 @@ func uploadToGCS(filename string, content []byte) error {
 	}
 
 	return nil
+}
+
+func downloadHandler(w http.ResponseWriter, r *http.Request) {
+	// Get the filename from the request query parameters
+	filename := r.URL.Query().Get("filename")
+	if filename == "" {
+		listFilesHandler(w, r)
+		return
+	}
+
+	// Set the Content-Disposition header to trigger a download on the client's browser
+	w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=\"%s\"", filename))
+
+	// Set up your Google Cloud Storage client
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return
+	}
+	defer client.Close()
+
+	// Create a new bucket handle
+	bucket := client.Bucket("ankur_docs")
+
+	// Create a new object handle
+	obj := bucket.Object(filename)
+
+	// Create a reader to download the file content
+	reader, err := obj.NewReader(ctx)
+	if err != nil {
+		http.Error(w, "Failed to create reader", http.StatusInternalServerError)
+		return
+	}
+	defer reader.Close()
+
+	// Copy the file content to the response writer
+	_, err = io.Copy(w, reader)
+	if err != nil {
+		http.Error(w, "Failed to copy file content", http.StatusInternalServerError)
+		return
+	}
+}
+
+type GCSFile struct {
+	Name     string
+	FileSize string
+}
+
+func listFiles() []GCSFile {
+	// Set up your Google Cloud Storage client
+	ctx := context.Background()
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		return nil
+	}
+	defer client.Close()
+
+	// Create a new bucket handle
+	bucket := client.Bucket("ankur_docs")
+
+	// List all objects in the bucket
+	it := bucket.Objects(ctx, nil)
+
+	var files []GCSFile
+	for {
+		objAttrs, err := it.Next()
+		if err == iterator.Done {
+			break
+		}
+		if err != nil {
+			return nil
+		}
+
+		files = append(files, GCSFile{
+			Name:     objAttrs.Name,
+			FileSize: fmt.Sprintf("%d bytes", objAttrs.Size),
+		})
+	}
+
+	return files
+}
+
+func listFilesHandler(w http.ResponseWriter, r *http.Request) {
+	// Set up your Google Cloud Storage client
+	files := listFiles()
+	// Create a template for the HTML content
+	htmlTemplate := `<html><body><ul>{{range .}}<li><a href="/dynamic?func=download&filename={{.Name}}">{{.Name}}</a> ({{.FileSize}})</li>{{end}}</ul></body></html>`
+	// Create a new buffer to store the rendered HTML
+	var buf bytes.Buffer
+	// Execute the template with the files as the data
+	tmpl := template.Must(template.New("html").Parse(htmlTemplate))
+	err := tmpl.Execute(&buf, files)
+	if err != nil {
+		http.Error(w, "Failed to render HTML", http.StatusInternalServerError)
+		return
+	}
+	// Write the rendered HTML to the response writer
+	fmt.Fprint(w, buf.String())
 }
